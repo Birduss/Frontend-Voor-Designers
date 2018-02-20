ea7c786e17fb4cd8aa3afa72e81caa9e0dcc41ce
Bart <bartrijnen8@gmail.com>
2018-02-20 01:23:29 +0100

    f

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,15 @@
 var button = document.querySelector('.meerinfo');
 var button2 = document.querySelector('.meerinfo2');
+var button3 = document.querySelector('.meerinfo3');
 var dropdown = document.querySelector('.info');
 var dropdown2 = document.querySelector('.info2');
+var dropdown3 = document.querySelector('.info3');
 var tussenruimte = document.querySelector('.ruimte');
+var tussenruimte2 = document.querySelector('.ruimte2');
+var tussenruimte3 = document.querySelector('.ruimte3');
 var pijltje = document.querySelector('.pijltje');
 var pijltje2 = document.querySelector('.pijltje2');
-
+var pijltje3 = document.querySelector('.pijltje3');
 
 
 button.addEventListener('click', voegclasstoe)
@@ -22,8 +26,24 @@ button2.addEventListener('click', voegclasstoe2)
 function voegclasstoe2(){
 //    console.log('click');
     dropdown2.classList.toggle('deinfo2');
+    tussenruimte2.classList.toggle('vervangruimte2');
     pijltje2.classList.toggle('omgedraaid2');
 }
+
+button3.addEventListener('click', voegclasstoe3)
+
+function voegclasstoe3(){
+//    console.log('click');
+    dropdown3.classList.toggle('deinfo3');
+    tussenruimte3.classList.toggle('vervangruimte3');
+    pijltje3.classList.toggle('omgedraaid3');
+}
+
+
+
+
+
+
 
 
 
